46a8977542590e3b298e2d72ef36a59befd2db84
Johannes <kulzer.johannes@tutanota.com>
2022-05-27 13:02:08 +0200

    changed argocd init release name and modified ingress

--- argocd/init-argocd.sh
+++ argocd/init-argocd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-helm template argo-cd argocd/argo-cd --include-crds -n argocd --values values.yaml \
+helm template argocd argocd/argo-cd --include-crds -n argocd --values values.yaml \
     | kubectl apply -n argocd -f -
