25f3181b74778adda346e23cb8f130877e4c4c6e
jkulzer <kulzer.johannes@tutanota.com>
2022-08-02 19:22:15 +0200

    added metallb

--- cluster/config/metallb/init-metallb.sh
+++ cluster/config/metallb/init-metallb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-helm template metallb metallb/metallb --include-crds -n argocd --values values.yaml \
+helm template metallb metallb/metallb --include-crds -n metallb-system --values values.yaml \
     | kubectl apply -n metallb-system -f -
