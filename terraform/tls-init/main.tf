3088c2d583ad15c936eb674503e4a588dbad3d3c
jkulzer <kulzer.johannes@tutanota.com>
2023-05-19 14:13:19 +0200

    added certificate initialization for vault via terraform

--- terraform/tls-init/main.tf
+++ terraform/tls-init/main.tf
@@ -13,3 +13,17 @@ terraform {
 
 
 }
+
+
+variable "vault_addr" {
+type = string
+}
+
+variable "vault-root" {
+type = string
+}
+
+provider "vault" {
+  address = var.vault_addr
+  token = var.vault-root
+}
