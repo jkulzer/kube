8a3031e7c0b1b2296d00ae74abff1130d9ea6978
jkulzer <kulzer.johannes@tutanota.com>
2023-06-14 14:48:16 +0200

    reduced initial delay of authentik-server to 80 seconds

--- cluster/config/authentik/authentik-get-initial-password.sh
+++ cluster/config/authentik/authentik-get-initial-password.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "The default password is akadmin"
+#sets the correct vault token
+export VAULT_TOKEN=$(kubectl get secret -n vault vault-unseal-keys -o jsonpath="{.data.vault-root}" | base64 -d)
 
 #Prints the password
 vault kv get -field=initialadminpassword kv/kube/authentik
