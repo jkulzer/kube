b30bf2b78573afb2473c979621d454e61d24170c
jkulzer <kulzer.johannes@tutanota.com>
2023-08-20 16:08:45 +0200

    added patch to talos config init

--- talos/init.sh
+++ talos/init.sh
@@ -1,5 +1,5 @@
 talosctl gen secrets -o secrets.yaml
-talosctl gen config --with-secrets secrets.yaml homeprod01 https://node1.nodes.kube.home:6443
+talosctl gen config --with-secrets secrets.yaml homeprod01 https://node1.nodes.kube.home:6443 --config-patch @patch.yaml
 talosctl apply-config --insecure -n node1.nodes.kube.home --file controlplane.yaml
 talosctl bootstrap -n 192.168.42.33 --talosconfig talosconfig
 talosctl kubeconfig --talosconfig talosconfig -n 192.168.42.33
