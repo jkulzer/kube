548e0990c7293eebce060d5de0a297a4f9ff7e99
jkulzer <kulzer.johannes@tutanota.com>
2023-08-20 16:07:52 +0200

    removed authentik

--- cluster/config/authentik/authentik-get-initial-password.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#sets the correct vault token
-export VAULT_TOKEN=$(kubectl get secret -n vault vault-unseal-keys -o jsonpath="{.data.vault-root}" | base64 -d)
-
-#Prints the password
-vault kv get -field=initialadminpassword kv/kube/authentik
