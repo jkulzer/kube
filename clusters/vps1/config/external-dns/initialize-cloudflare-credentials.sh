9479d94954e69a783b4a0cf21c9799e4398522e4
jkulzer <git@jkulzer.dev>
2023-10-07 19:28:58 +0000

    switched name to prod issuer

--- clusters/vps1/config/external-dns/initialize-cloudflare-credentials.sh
+++ clusters/vps1/config/external-dns/initialize-cloudflare-credentials.sh
@@ -5,4 +5,4 @@ read -p "Enter Cloudflare API key: " cloudflare_api_key
 read -p "Enter Cloudflare EMail Address: " cloudflare_mail_address
 
 # Write the secret
-kubectl create secret generic -n external-dns cloudflare-credentials --from-literal="CF_API_KEY=$cloudflare_api_key" --from-literal="CF_API_EMAIL=$cloudflare_mail_address"
+kubectl create secret generic -n external-dns cloudflare-credentials --from-literal="CF_API_TOKEN=$cloudflare_api_key" --from-literal="CF_API_EMAIL=$cloudflare_mail_address"
