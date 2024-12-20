de67e17b96763ca0ddd143a076314995793e3a5c
jkulzer <git@jkulzer.dev>
2024-12-20 23:45:05 +0100

    removed private authelia

--- clusters/hybrid01/config/authelia-private/initialize-mailgun-credentials.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -p "Username: " api_key
-
-read -p "Password: " api_secret
-
-# Write the secret
-kubectl create secret generic -n authelia smtp-credentials --from-literal="SMTP_USERNAME=$api_key" --from-literal="SMTP_PASSWORD=$api_secret"
